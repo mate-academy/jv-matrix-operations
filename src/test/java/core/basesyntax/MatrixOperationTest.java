65ca397ed5815d2c04478b92fe1ed0a42b827258
Sofasmile <sofiya.dyakonova@ukr.net>
2019-08-15 13:07:35 +0300

    divided tests

--- src/test/java/core/basesyntax/MatrixOperationTest.java
+++ src/test/java/core/basesyntax/MatrixOperationTest.java
@@ -6,76 +6,73 @@ import org.junit.Test;
 import java.util.Arrays;
 
 public class MatrixOperationTest {
-    private static final int[][] FIRST_TEST_MATRIX = {
-            {2, 31, 6, 7},
-            {15, 42, 1, 0},
-            {9, 7, 12, 19},
-            {55, 34, 1, 10}
+    private static final int[][] TEST_MATRIX = {
+            {-2, 31, 6, 7},
+            {15, -42, 1, 0},
+            {9, -7, 12, 19},
+            {55, 34, 1, -10}
     };
-    private static final int[][] SECOND_TEST_MATRIX = {
-            {-2, 281, 0},
-            {-15, 0, 1},
-            {3, 7, -12},
-    };
-    private static final int[][] THIRD_TEST_MATRIX = {
+
+    private static final int[][] SINGLE_ELEMENT_MATRIX = {
             {-5}
     };
 
-    private static final int[] FIRST_EXPECTED_DIAGONAL = {2, 42, 12, 10};
-    private static final int[] SECOND_EXPECTED_DIAGONAL = {-2, 0, -12};
-    private static final int[] THIRD_EXPECTED_DIAGONAL = {-5};
+    private static final int[] MATRIX_EXPECTED_DIAGONAL = {-2, -42, 12, -10};
+    private static final int[] SINGLE_ELEMENT_EXPECTED_DIAGONAL = {-5};
 
     @Test
-    public void getDiagonal() {
+    public void getMatrixDiagonal() {
         MatrixOperation matrixOperation = new MatrixOperation();
-        int[] firstActualResult = matrixOperation.getDiagonal(FIRST_TEST_MATRIX);
-        int[] secondActualResult = matrixOperation.getDiagonal(SECOND_TEST_MATRIX);
-        int[] thirdActualResult = matrixOperation.getDiagonal(THIRD_TEST_MATRIX);
+        int[] actualResult = matrixOperation.getDiagonal(TEST_MATRIX);
         Assert.assertArrayEquals("Test failed with result = "
-                + Arrays.toString(firstActualResult), firstActualResult, FIRST_EXPECTED_DIAGONAL);
-        Assert.assertArrayEquals("Test failed with result = "
-                + Arrays.toString(secondActualResult), secondActualResult, SECOND_EXPECTED_DIAGONAL);
-        Assert.assertArrayEquals("Test failed with result = "
-                + Arrays.toString(thirdActualResult), thirdActualResult, THIRD_EXPECTED_DIAGONAL);
+                + Arrays.toString(actualResult), actualResult, MATRIX_EXPECTED_DIAGONAL);
     }
 
     @Test
-    public void getMin() {
+    public void getSingleElementMatrixDiagonal() {
         MatrixOperation matrixOperation = new MatrixOperation();
-        int firstActualResult = matrixOperation.getMin(FIRST_TEST_MATRIX);
-        int firstExpectedResult = Arrays.stream(FIRST_TEST_MATRIX)
-                .flatMapToInt(Arrays::stream).min().getAsInt();
-        int secondActualResult = matrixOperation.getMin(SECOND_TEST_MATRIX);
-        int secondExpectedResult = Arrays.stream(SECOND_TEST_MATRIX)
-                .flatMapToInt(Arrays::stream).min().getAsInt();
-        int thirdActualResult = matrixOperation.getMin(THIRD_TEST_MATRIX);
-        int thirdExpectedResult = Arrays.stream(THIRD_TEST_MATRIX)
-                .flatMapToInt(Arrays::stream).min().getAsInt();
-        Assert.assertEquals("Test failed with min result = "
-                + firstActualResult, firstActualResult, firstExpectedResult);
-        Assert.assertEquals("Test failed with min result = "
-                + secondActualResult, secondActualResult, secondExpectedResult);
-        Assert.assertEquals("Test failed with min result = "
-                + thirdActualResult, thirdActualResult, thirdExpectedResult);
+        int[] actualResult = matrixOperation.getDiagonal(SINGLE_ELEMENT_MATRIX);
+        Assert.assertArrayEquals("Test failed with result = "
+                + Arrays.toString(actualResult), actualResult, SINGLE_ELEMENT_EXPECTED_DIAGONAL);
     }
 
     @Test
-    public void getMax() {
+    public void getMatrixMin() {
         MatrixOperation matrixOperation = new MatrixOperation();
-        int firstActualResult = matrixOperation.getMax(FIRST_TEST_MATRIX);
-        int firstExpectedResult = Arrays.stream(FIRST_TEST_MATRIX)
-                .flatMapToInt(Arrays::stream).max().getAsInt();
-        int secondActualResult = matrixOperation.getMax(SECOND_TEST_MATRIX);
-        int secondExpectedResult = Arrays.stream(SECOND_TEST_MATRIX)
-                .flatMapToInt(Arrays::stream).max().getAsInt();
-        int thirdActualResult = matrixOperation.getMax(THIRD_TEST_MATRIX);
-        int thirdExpectedResult = Arrays.stream(THIRD_TEST_MATRIX)
+        int actualResult = matrixOperation.getMin(TEST_MATRIX);
+        int expectedResult = Arrays.stream(TEST_MATRIX)
+                .flatMapToInt(Arrays::stream).min().getAsInt();
+        Assert.assertEquals("Test failed with min result = "
+                + actualResult, actualResult, expectedResult);
+    }
+
+    @Test
+    public void getSingleElementMatrixMin() {
+        MatrixOperation matrixOperation = new MatrixOperation();
+        int actualResult = matrixOperation.getMin(SINGLE_ELEMENT_MATRIX);
+        int expectedResult = Arrays.stream(SINGLE_ELEMENT_MATRIX)
+                .flatMapToInt(Arrays::stream).min().getAsInt();
+        Assert.assertEquals("Test failed with min result = "
+                + actualResult, actualResult, expectedResult);
+    }
+
+    @Test
+    public void getMatrixMax() {
+        MatrixOperation matrixOperation = new MatrixOperation();
+        int actualResult = matrixOperation.getMax(TEST_MATRIX);
+        int expectedResult = Arrays.stream(TEST_MATRIX)
                 .flatMapToInt(Arrays::stream).max().getAsInt();
         Assert.assertEquals("Test failed with max result = "
-                + firstActualResult, firstActualResult, firstExpectedResult);
+                + actualResult, actualResult, expectedResult);
+    }
+
+    @Test
+    public void getSingleElementMatrixMax() {
+        MatrixOperation matrixOperation = new MatrixOperation();
+        int actualResult = matrixOperation.getMax(SINGLE_ELEMENT_MATRIX);
+        int expectedResult = Arrays.stream(SINGLE_ELEMENT_MATRIX)
+                .flatMapToInt(Arrays::stream).max().getAsInt();
         Assert.assertEquals("Test failed with max result = "
-                + secondActualResult, secondActualResult, secondExpectedResult);
-        Assert.assertEquals("Test failed with max result = "
-                + thirdActualResult, thirdActualResult, thirdExpectedResult);
+                + actualResult, actualResult, expectedResult);
     }
 }
