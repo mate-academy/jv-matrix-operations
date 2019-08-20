d1a5958b4301bccba8602632366d74058fe99228
Max <skaterweather41@gmail.com>
2019-08-20 17:54:02 +0200

    added solution to task 4 matrixOperations

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -1,19 +1,39 @@
 package core.basesyntax;
-
 /**
  * Реалізуйте методи для знаходження головної діагоналі прямокутної матриці,
  * максимального та мінімального елементів.
  */
+
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] res = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            res[i] = matrix[i][i];
+        }
+        return res;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int[] slice : matrix) {
+            for (int element : slice) {
+                if (element < min) {
+                    min = element;
+                }
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int[] slice : matrix) {
+            for (int element : slice) {
+                if (element > max) {
+                    max = element;
+                }
+            }
+        }
+        return max;
     }
 }
