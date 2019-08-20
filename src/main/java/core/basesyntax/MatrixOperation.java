81a3dab4817c080be14c44401dfd2637f54f1973
Sviatoslav <s.granovskiy@gmail.com>
2019-08-20 22:11:31 +0300

    hw-007-solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,15 +5,46 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
-    public int[] getDiagonal(int[][] matrix) {
-        return null;
+    protected int[] getDiagonal(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] result;
+        if (row < col) {
+            result = new int[row];
+        } else {
+            result = new int[col];
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == j) {
+                    result[i] = matrix[i][j];
+                }
+            }
+        }
+        return result;
     }
 
-    public int getMin(int[][] matrix) {
-        return 0;
+    protected int getMin(int[][] matrix) {
+        int minimum = Integer.MAX_VALUE;
+        for (int[] numbers : matrix) {
+            for (int number : numbers) {
+                if (number < minimum) {
+                    minimum = number;
+                }
+            }
+        }
+        return minimum;
     }
 
-    public int getMax(int[][] matrix) {
-        return 0;
+    protected int getMax(int[][] matrix) {
+        int maximum = Integer.MIN_VALUE;
+        for (int[] numbers : matrix) {
+            for (int number : numbers) {
+                if (number > maximum) {
+                    maximum = number;
+                }
+            }
+        }
+        return maximum;
     }
 }
