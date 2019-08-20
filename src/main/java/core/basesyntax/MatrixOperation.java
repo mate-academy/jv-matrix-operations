efed1ff9ae3d2ddcb397dac0db1c9cff8cc8039b
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-20 19:00:29 +0300

    Update MatrixOperation.java

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -1,19 +1,50 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Реалізуйте методи для знаходження головної діагоналі прямокутної матриці,
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
-    public int[] getDiagonal(int[][] matrix) {
-        return null;
+    protected int[] getDiagonal(int[][] matrix) {
+        int diagonal[] = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            diagonal[i] = matrix[i][i];
+        }
+        return diagonal;
     }
 
-    public int getMin(int[][] matrix) {
-        return 0;
+    protected int getMin(int[][] matrix) {
+        List<Integer> oneDMatrix = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                oneDMatrix.add(matrix[i][j]);
+            }
+        }
+        int min = oneDMatrix.get(0);
+        for (int i : oneDMatrix) {
+            if (min > i) {
+                min = i;
+            }
+        }
+        return min;
     }
 
-    public int getMax(int[][] matrix) {
-        return 0;
+    protected int getMax(int[][] matrix) {
+        List<Integer> oneDMatrix = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                oneDMatrix.add(matrix[i][j]);
+            }
+        }
+        int max = oneDMatrix.get(0);
+        for (int i : oneDMatrix) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        return max;
     }
 }
