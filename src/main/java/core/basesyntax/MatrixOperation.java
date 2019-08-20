df3bb54312dbabd8695f62b87380465f1a87fe39
roman_minevich <283389dv3qsh>
2019-08-20 15:39:13 +0300

    Add solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,14 +6,34 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] diagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            diagonal[i] = matrix[i][i];
+        }
+        return diagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int ii = 0; ii < matrix[i].length; ii++) {
+                if (matrix[i][ii] < min) {
+                    min = matrix[i][ii];
+                }
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int ii = 0; ii < matrix[i].length; ii++) {
+                if (matrix[i][ii] > max) {
+                    max = matrix[i][ii];
+                }
+            }
+        }
+        return max;
     }
 }
