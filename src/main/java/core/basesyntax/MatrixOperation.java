eb1d86b63db10f3031c0d1b716a07d5d9a66cc2e
kmety <kmety9876@gmail.com>
2019-08-20 19:20:58 +0300

    solution added

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -1,19 +1,54 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Реалізуйте методи для знаходження головної діагоналі прямокутної матриці,
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
-    public int[] getDiagonal(int[][] matrix) {
-        return null;
+    public static int[] getDiagonal(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        int i;
+        int j;
+        for (i = 0, j = 0; i < matrix.length && j < matrix[i].length; i++, j++) {
+            list.add(matrix[i][j]);
+        }
+        int[] result = new int[list.size()];
+        for (i = 0; i < result.length; i++) {
+            result[i] = list.get(i).intValue();
+        }
+        return result;
     }
 
-    public int getMin(int[][] matrix) {
-        return 0;
+    public static int getMin(int[][] matrix) {
+        if (matrix.length == 0 || matrix == null) {
+            return 0;
+        }
+        int minValue = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < minValue) {
+                    minValue = matrix[i][j];
+                }
+            }
+        }
+        return minValue;
     }
 
-    public int getMax(int[][] matrix) {
-        return 0;
+    public static int getMax(int[][] matrix) {
+        if (matrix.length == 0 || matrix == null) {
+            return 0;
+        }
+        int maxValue = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > maxValue) {
+                    maxValue = matrix[i][j];
+                }
+            }
+        }
+        return maxValue;
     }
 }
