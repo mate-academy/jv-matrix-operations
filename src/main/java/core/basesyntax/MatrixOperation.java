6eeb853f9242c0ebf74282b45431b6b57ae29dcb
Тарас <tarashryts@gmail.com>
2019-08-20 16:12:20 +0300

    add hw-2-solution-matrix

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,15 +5,42 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
-    public int[] getDiagonal(int[][] matrix) {
-        return null;
+    protected int[] getDiagonal(int[][] matrix) {
+        int dementionX = matrix.length;
+        int dementionY = matrix[0].length;
+        int minDemention = dementionX < dementionY ? dementionX : dementionY;
+        int[] arrayMainDiagonal = new int[minDemention];
+        for (int i = 0; i < minDemention; i++) {
+            arrayMainDiagonal[i] = matrix[i][i];
+        }
+        return arrayMainDiagonal;
     }
 
-    public int getMin(int[][] matrix) {
-        return 0;
+    protected int getMin(int[][] matrix) {
+        int minElement = matrix[0][0];
+        int dementionX = matrix.length;
+        int dementionY = matrix[0].length;
+        for (int i = 0; i < dementionX; i++) {
+            for (int j = 0; j < dementionY; j++) {
+                if (matrix[i][j] < minElement) {
+                    minElement = matrix[i][j];
+                }
+            }
+        }
+        return minElement;
     }
 
-    public int getMax(int[][] matrix) {
-        return 0;
+    protected int getMax(int[][] matrix) {
+        int maxElement = matrix[0][0];
+        int dementionX = matrix.length;
+        int dementionY = matrix[0].length;
+        for (int i = 0; i < dementionX; i++) {
+            for (int j = 0; j < dementionY; j++) {
+                if (matrix[i][j] > maxElement) {
+                    maxElement = matrix[i][j];
+                }
+            }
+        }
+        return maxElement;
     }
 }
