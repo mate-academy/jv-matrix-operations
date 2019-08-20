dca72ab6d962929901e047dcfe5a98f4aaa6951f
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-20 14:58:16 +0300

    implemented getDiagonal, getMin and getMax methods in MatrixOperation class

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,15 +5,54 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
+    /**
+     * Returns an array of elements of the main diagonal of the matrix.
+     *
+     * @param matrix double array that is a representation of a matrix
+     * @return array of elements of the main diagonal of the matrix
+     */
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int diagonalCapacity = Math.min(matrix.length, matrix[0].length);
+        int[] diogonal = new int[diagonalCapacity];
+        for (int i = 0; i < diagonalCapacity; i++) {
+            diogonal[i] = matrix[i][i];
+        }
+        return diogonal;
     }
 
+    /**
+     * Returns min element of the matrix.
+     *
+     * @param matrix double array that is a representation of a matrix
+     * @return min element of the matrix
+     */
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        return min;
     }
 
+    /**
+     * Returns max element of the matrix.
+     *
+     * @param matrix double array that is a representation of a matrix
+     * @return max element of the matrix
+     */
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
     }
 }
