e00c462e281c53401a4f1429fd3dcb0585908cb3
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-20 20:39:37 +0300

    realize all methods

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,14 +6,48 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int arraylength = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    arraylength++;
+                }
+            }
+        }
+        int[] array = new int[arraylength];
+        for (int j = 0; j < matrix.length; j++) {
+            for (int k = 0; k < matrix[0].length; k++) {
+                if (j == k) {
+                    array[j] = matrix[j][k];
+                }
+            }
+        }
+        return array;
     }
 
+
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            max = matrix[i][0];
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (max < matrix[i][j]) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
     }
 }
