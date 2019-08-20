c19f73741e55fb034ef952a3d655f95c5857b88c
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-20 13:25:00 +0300

    added solution for matrix homework

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,15 +5,44 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
+    /**
+     * This function returns
+     * diagonal of square matrix.
+     */
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int resLength = matrix.length;
+        int[] result = new int[resLength];
+        for (int i = 0; i < resLength; i++) {
+            result[i] = matrix[i][i];
+        }
+        return result;
     }
 
+    /**
+     * This function returns
+     * minimal element of matrix.
+     */
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int[] arr : matrix) {
+            for (int num : arr) {
+                min = Math.min(min, num);
+            }
+        }
+        return min;
     }
 
+    /**
+     * This function returns
+     * maximal element of matrix.
+     */
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int[] arr : matrix) {
+            for (int num : arr) {
+                max = Math.max(max, num);
+            }
+        }
+        return max;
     }
 }
