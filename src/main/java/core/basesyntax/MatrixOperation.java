dba5dcd4875c8be264c85db9b3fe26d5f101790e
Iurii <kenu21@gmail.com>
2019-08-20 21:49:58 +0300

    added hw-2-4-solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,14 +6,43 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int size1 = matrix.length;
+        int size2 = matrix[size1 - 1].length;
+        int size = (size1 > size2) ? size2 : size1;
+
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = matrix[i][i];
+        }
+
+        return result;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+
+        for (int[] i : matrix) {
+            for (int j : i) {
+                if (j < min) {
+                    min = j;
+                }
+            }
+        }
+
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+
+        for (int[] i : matrix) {
+            for (int j : i) {
+                if (j > max) {
+                    max = j;
+                }
+            }
+        }
+
+        return max;
     }
 }
