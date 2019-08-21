2615ff48e6f6d2b1f92e43ecfb26b8b45b98afd5
Andrew <tolstuy1996@gmail.com>
2019-08-21 12:55:52 +0300

    fixed selection fo matrix

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -7,43 +7,33 @@ package core.basesyntax;
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
         int[] mainDiagonl = new int[matrix.length];
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (i == j) {
-                    mainDiagonl[i] = matrix[i][j];
-                }
-            }
+        for (int i = 0; i < matrix.length && matrix[i].length > i; i++) {
+            mainDiagonl[i] = matrix[i][i];
         }
-
         return mainDiagonl;
     }
 
     public int getMin(int[][] matrix) {
-        int tempMin = 0;
-
-        for (int i = 0; i < matrix.length; i++) {
+        int tempMin = matrix[0][0];
+        for (int i = 1; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] < tempMin) {
                     tempMin = matrix[i][j];
                 }
             }
         }
-
         return tempMin;
     }
 
     public int getMax(int[][] matrix) {
-        int tempMax = -5;
-
-        for (int i = 0; i < matrix.length; i++) {
+        int tempMax = matrix[0][0];
+        for (int i = 1; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] > tempMax) {
                     tempMax = matrix[i][j];
                 }
             }
         }
-
         return tempMax;
     }
 }
