6fce0945f0c6b6ae5b69e0f295cd68981e8cd02d
Valdes2 <52889938+Valdes2@users.noreply.github.com>
2019-08-21 07:31:31 +0300

    added solution for homework MatrixOperation

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -1,19 +1,51 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 /**
  * Реалізуйте методи для знаходження головної діагоналі прямокутної матриці,
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int [] diagonal = new int[matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == j) {
+                    diagonal[i] = matrix[i][j];
+                }
+            }
+        }
+
+        return diagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+
+        int [] flat = new int [matrix.length * matrix[0].length];
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                flat[count++] = matrix[i][j];
+            }
+        }
+        Arrays.sort(flat);
+
+        return flat[0];
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+
+        int [] flat = new int [matrix.length * matrix[0].length];
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                flat[count++] = matrix[i][j];
+            }
+        }
+        Arrays.sort(flat);
+
+        return flat[flat.length - 1];
     }
 }
