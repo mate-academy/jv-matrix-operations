5112f9579646496fbdef67571890004a488b89d2
Andrew <tolstuy1996@gmail.com>
2019-08-21 20:03:43 +0300

    fixed

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -7,33 +7,33 @@ package core.basesyntax;
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
         int[] mainDiagonl = new int[matrix.length];
-        for (int i = 0; i < matrix.length && matrix[i].length > i; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             mainDiagonl[i] = matrix[i][i];
         }
         return mainDiagonl;
     }
 
     public int getMin(int[][] matrix) {
-        int tempMin = matrix[0][0];
+        int min = matrix[0][0];
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] < tempMin) {
-                    tempMin = matrix[i][j];
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
                 }
             }
         }
-        return tempMin;
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        int tempMax = matrix[0][0];
+        int max = matrix[0][0];
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] > tempMax) {
-                    tempMax = matrix[i][j];
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
                 }
             }
         }
-        return tempMax;
+        return max;
     }
 }
