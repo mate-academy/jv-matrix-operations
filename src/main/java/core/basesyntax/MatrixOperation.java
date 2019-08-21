3df2c35a4edb5b8c1c1e07a1e9639fa0e50a53ef
admin2k7@gmail.com <admin2k7@gmail.com>
2019-08-21 22:19:14 +0300

    2.4 task added

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,14 +6,37 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] diagonal = new int[matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            diagonal[i] = matrix[i][i];
+        }
+        return diagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int minElement = matrix[0][0];
+
+        for (int[] i : matrix) {
+            for (int j : i) {
+                if (j < minElement) {
+                    minElement = j;
+                }
+            }
+        }
+        return minElement;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int maxElement = matrix[0][0];
+
+        for (int[] i : matrix) {
+            for (int j : i) {
+                if (j > maxElement) {
+                    maxElement = j;
+                }
+            }
+        }
+        return maxElement;
     }
 }
