6d9cd901ded7eb7785e073182b89fc2b51284465
Саша Бунин <sasha192.bunin@gmail.com>
2019-08-21 23:03:04 +0300

    Update MatrixOperation.java

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,7 +6,7 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     protected int[] getDiagonal(int[][] matrix) {
-        int size = matrix[0].length > matrix.length ? matrix.length : matrix[0].length;
+        int size = matrix.length;
         int[] returnArray = new int[size];
         for (int i = 0; i < size; i++) {
             returnArray[i] = matrix[i][i];
@@ -17,7 +17,7 @@ public class MatrixOperation {
     protected int getMin(int[][] matrix) {
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+            for (int j = 0; j < matrix.length; j++) {
                 min = min > matrix[i][j] ? matrix[i][j] : min;
             }
         }
@@ -27,7 +27,7 @@ public class MatrixOperation {
     protected int getMax(int[][] matrix) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+            for (int j = 0; j < matrix.length; j++) {
                 max = max < matrix[i][j] ? matrix[i][j] : max;
             }
         }
