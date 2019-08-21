83e4160855837b548aa5146c347fcd1a6d5729a5
lenovo <suleimanovayev@gmail.com>
2019-08-21 15:27:39 +0300

    fixed

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -4,16 +4,32 @@ package core.basesyntax;
  * Реалізуйте методи для знаходження головної діагоналі прямокутної матриці,
  * максимального та мінімального елементів.
  */
-public class MatrixOperation {
+ public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] getDiagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            getDiagonal[i] = matrix[i][i];
+        }
+        return getDiagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                min = (min > matrix[i][j]) ? matrix[i][j] : min;
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int a = 0; a < matrix.length; a++) {
+            for (int j = 0; j < matrix.length; j++) {
+                max = (max < matrix[a][j]) ? matrix[a][j] : max;
+            }
+        }
+        return max;
     }
-}
+ }
