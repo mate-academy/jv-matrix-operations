fdbd21aa170251be543fa86d2b4dfeb7279f66f2
Grave <graveman13@gmail.com>
2019-12-03 12:59:04 +0200

    home work 2 jv-matrix-operations

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,19 +5,47 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
+
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] diagonal = new int[matrix[0].length];
+        for (int i = matrix.length - 1;i >= 0;i--) {
+            diagonal[i] = matrix[i][i];
+        }
+        return diagonal;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] diagonal = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            diagonal[i] = matrix[i][matrix.length - 1 - i];
+        }
+        return diagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int n = matrix[i][j];
+                if (n < min) {
+                    min = n;
+                }
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int n = matrix[i][j];
+                if (n > max) {
+                    max = n;
+                }
+            }
+        }
+        //new Arrays.stream(matrix).ge/
+        return max;
     }
 }
