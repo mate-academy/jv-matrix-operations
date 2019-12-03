d68b95f70b4c2fc26889fcee59f79f15c3f17a11
Alexandr Sashchenko <santys87@gmail.com>
2019-12-03 19:54:26 +0200

    Added Mate homework 2.2 Solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,19 +5,58 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
+
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        if (matrix[0].length == 1) {
+            return matrix[0];
+        }
+        int[] diagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    diagonal[i] = matrix[i][j];
+                }
+            }
+        }
+        return diagonal;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        if (matrix[0].length == 1) {
+            return matrix[0];
+        }
+        int[] counterDiagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = matrix[i].length - 1; j >= 0; j--) {
+                if (matrix.length - i == j + 1) {
+                    counterDiagonal[i] = matrix[i][j];
+                }
+            }
+        }
+        return counterDiagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int minElement = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < minElement) {
+                    minElement = matrix[i][j];
+                }
+            }
+        }
+        return minElement;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int maxElement = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > maxElement) {
+                    maxElement = matrix[i][j];
+                }
+            }
+        }
+        return maxElement;
     }
 }
