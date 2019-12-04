4549eeab7be646760ba8e76a756f50e062dc1347
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2019-12-04 15:17:27 +0200

    task done

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,19 +5,52 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
+
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+
+        int[] mainDiag = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    mainDiag[i] = matrix[i][j];
+                }
+            }
+        }
+        return mainDiag;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+
+        int[] secDiag = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            secDiag[i] = matrix[i][matrix.length - 1 - i];
+        }
+        return secDiag;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+
+        int minVal = 2147483647;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (minVal > matrix[i][j]) {
+                    minVal = matrix[i][j];
+                }
+            }
+        }
+        return minVal;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+
+        int maxVal = -2147483648;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (maxVal < matrix[i][j]) {
+                    maxVal = matrix[i][j];
+                }
+            }
+        }
+        return maxVal;
     }
 }
