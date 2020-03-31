cc6c4c92da0e8b77e97a51d98320a13c133ca4db
SpringWay <44027294+SpringWay@users.noreply.github.com>
2020-03-31 15:54:46 +0300

    added solution to hw2.2

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,19 +5,45 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
+
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+
+        int[] counter = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            counter[i] = matrix[i][i];
+        }
+        return counter;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] counter = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            counter[i] = matrix[i][matrix.length - i - 1];
+        }
+        return counter;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
     }
 }
