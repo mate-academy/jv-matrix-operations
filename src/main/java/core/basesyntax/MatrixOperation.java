059fcb87797e3dae477fa4e04a4f51911e42b75e
Filipp <filipp888@gmail.com>
2020-03-31 13:15:44 +0200

    Added solution for Task 2.2

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,18 +6,45 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] mainDiag = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++){
+            mainDiag[i] = matrix[i][i];
+        }
+        return mainDiag;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] counterDiag = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++){
+            for (int y = 0; y < matrix.length; y++){
+                counterDiag[i] = matrix[i][matrix.length-i-1];
+            }
+
+        }
+        return counterDiag;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for(int i = 0; i < matrix.length; i++){
+            for (int y = 0; y < matrix[0].length; y++){
+                if (matrix[i][y] < min){
+                    min = matrix[i][y];
+                    }
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for(int i = 0; i < matrix.length; i++) {
+            for (int y = 0; y < matrix[0].length ; y++) {
+                if (matrix[i][y] > max) {
+                    max = matrix[i][y];
+                }
+            }
+        }
+        return max;
     }
 }
