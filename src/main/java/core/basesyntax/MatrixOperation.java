f0c48d37260d1cd5d7a90589b754a695d9e101dd
Djeckkenni <djeck27kenni@gmail.com>
2020-03-31 19:15:25 +0300

    correct code

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -1,20 +1,15 @@
 package core.basesyntax;
 
-import java.util.Arrays;
-
 /**
  * Реалізуйте методи для знаходження головної та побічної діагоналі квадратної матриці,
  * максимального та мінімального елементів.
  */
+
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
         int[] diagonal = new int[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (i == j) {
-                    diagonal[i] = matrix[i][j];
-                }
-            }
+            diagonal[i] = matrix[i][i];
         }
         return diagonal;
     }
@@ -23,7 +18,7 @@ public class MatrixOperation {
         int[] counterDiagonal = new int[matrix.length];
         int j = matrix.length - 1;
         for (int i = 0; i < matrix.length; i++) {
-            counterDiagonal[i] += matrix[i][j];
+            counterDiagonal[i] = matrix[i][j];
             j--;
         }
         return counterDiagonal;
@@ -32,20 +27,32 @@ public class MatrixOperation {
     public int getMin(int[][] matrix) {
         int[] min = new int[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            Arrays.sort(matrix[i]);
-            min[i] += matrix[i][0];
+            sortMatrix(matrix[i]);
+            min[i] = matrix[i][0];
         }
-        Arrays.sort(min);
+        sortMatrix(min);
         return min[0];
     }
 
     public int getMax(int[][] matrix) {
         int[] max = new int[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            Arrays.sort(matrix[i]);
-            max[i] += matrix[i][matrix.length - 1];
+            sortMatrix(matrix[i]);
+            max[i] = matrix[i][matrix.length - 1];
         }
-        Arrays.sort(max);
+        sortMatrix(max);
         return max[max.length - 1];
+    }
+
+    public int[] sortMatrix(int[] matrix) {
+        int temp = 0;
+        for (int i = 0; i < matrix.length - 1; i++) {
+            if (matrix[i] > matrix[i + 1]) {
+                temp = matrix[i + 1];
+                matrix[i + 1] = matrix[i];
+                matrix[i] = temp;
+            }
+        }
+        return matrix;
     }
 }
