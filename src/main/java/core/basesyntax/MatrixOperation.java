50290f8166520524faa8c93285e4a0bed4de0931
Filipp <filipp888@gmail.com>
2020-03-31 13:31:21 +0200

    Added solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -7,7 +7,7 @@ package core.basesyntax;
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
         int[] mainDiag = new int[matrix.length];
-        for (int i = 0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             mainDiag[i] = matrix[i][i];
         }
         return mainDiag;
@@ -15,22 +15,19 @@ public class MatrixOperation {
 
     public int[] getCounterDiagonal(int[][] matrix) {
         int[] counterDiag = new int[matrix.length];
-        for (int i = 0; i < matrix.length; i++){
-            for (int y = 0; y < matrix.length; y++){
-                counterDiag[i] = matrix[i][matrix.length-i-1];
-            }
-
+        for (int i = 0; i < matrix.length; i++) {
+            counterDiag[i] = matrix[i][matrix[0].length - i - 1];
         }
         return counterDiag;
     }
 
     public int getMin(int[][] matrix) {
         int min = matrix[0][0];
-        for(int i = 0; i < matrix.length; i++){
-            for (int y = 0; y < matrix[0].length; y++){
-                if (matrix[i][y] < min){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int y = 0; y < matrix[0].length; y++) {
+                if (matrix[i][y] < min) {
                     min = matrix[i][y];
-                    }
+                }
             }
         }
         return min;
@@ -38,8 +35,8 @@ public class MatrixOperation {
 
     public int getMax(int[][] matrix) {
         int max = matrix[0][0];
-        for(int i = 0; i < matrix.length; i++) {
-            for (int y = 0; y < matrix[0].length ; y++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int y = 0; y < matrix[0].length; y++) {
                 if (matrix[i][y] > max) {
                     max = matrix[i][y];
                 }
