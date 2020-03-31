dcbf8c2bacc921385956e53df75f3b686e60d5b9
Polina Kulyk <polina.kulyk@protonmail.com>
2020-03-31 17:50:12 +0300

    rewrited ternary comparison to if loop

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -25,7 +25,7 @@ public class MatrixOperation {
         int min = matrix[0][0];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                if (min > matrix[i][j]) {
+                if (matrix[i][j] < min) {
                     min = matrix[i][j];
                 }
             }
@@ -37,7 +37,7 @@ public class MatrixOperation {
         int max = matrix[0][0];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                if (max < matrix[i][j]) {
+                if (matrix[i][j] > max) {
                     max = matrix[i][j];
                 }
             }
