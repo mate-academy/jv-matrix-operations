d67126811a602287a84d44975b128d0c9db0c566
vladyslav <vlad.bes0711@gmail.com>
2020-03-31 12:18:05 +0300

    hw 2.2 added solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,18 +6,40 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] diagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            diagonal[i] = matrix[i][i];
+        }
+        return diagonal;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] counterDiagonal = new int[matrix.length];
+        int j = matrix.length - 1;
+        for (int i = 0; i < matrix.length; i++) {
+            counterDiagonal[i] = matrix[i][j];
+            j--;
+        }
+        return counterDiagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int minElement = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                minElement = matrix[i][j] < minElement ? matrix[i][j] : minElement;
+            }
+        }
+        return minElement;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int maxElem = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                maxElem = matrix[i][j] > maxElem ? matrix[i][j] : maxElem;
+            }
+        }
+        return maxElem;
     }
 }
