84f8f972cbfc7abe4581c48125219d671314b575
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-03-31 13:22:57 +0300

    added solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,18 +6,42 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] result = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            result[i] = matrix[i][i];
+        }
+        return result;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] result = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            result[i] = matrix[i][matrix[0].length - i - 1];
+        }
+        return result;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int result = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < result) {
+                    result = matrix[i][j];
+                }
+            }
+        }
+        return result;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int result = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] > result) {
+                    result = matrix[i][j];
+                }
+            }
+        }
+        return result;
     }
 }
