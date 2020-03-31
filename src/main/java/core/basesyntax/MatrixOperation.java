33e0276e4a8ae1de3536850a10d21996962cf324
Allduiin <vovapro01@gmail.com>
2020-03-31 13:33:06 +0300

    Added solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,18 +6,52 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] ans = new int[matrix.length];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = matrix[i][i];
+        }
+        return ans;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] ans = new int[matrix.length];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = matrix[i][ans.length - 1 - i];
+        }
+        return ans;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        /* или можно так но второй вариант быстрее на одну операцию
+        int minn=matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                minn=Math.min(minn,matrix[i][j]);
+            }
+        }
+        */
+        int minn = matrix[0][0];
+        for (int i = 1; i < matrix[0].length; i++) {
+            minn = Math.min(minn, matrix[0][i]);
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                minn = Math.min(minn, matrix[i][j]);
+            }
+        }
+        return minn;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int maxx = matrix[0][0];
+        for (int i = 1; i < matrix[0].length; i++) {
+            maxx = Math.max(maxx, matrix[0][i]);
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                maxx = Math.max(maxx, matrix[i][j]);
+            }
+        }
+        return maxx;
     }
 }
