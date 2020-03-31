7faa1708023dfea3e217b141526e69a37d81a892
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-03-31 14:00:37 +0300

    added solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -5,19 +5,47 @@ package core.basesyntax;
  * максимального та мінімального елементів.
  */
 public class MatrixOperation {
+
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] result = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            result[i] = matrix[i][i];
+        }
+        return result;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] result = new int[matrix.length];
+        int j = 0;
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            result[j] = matrix[j][i];
+            j++;
+        }
+
+        return result;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int biggestNum = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (biggestNum > matrix[i][j]) {
+                    biggestNum = matrix[i][j];
+                }
+            }
+        }
+        return biggestNum;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int biggestNum = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (biggestNum < matrix[i][j]) {
+                    biggestNum = matrix[i][j];
+                }
+            }
+        }
+        return biggestNum;
     }
 }
