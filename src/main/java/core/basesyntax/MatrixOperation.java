d964e6c6a4281df4bc9f7545d7aa8759bc56e2b5
Andrey Tanchak <61573898+boterf@users.noreply.github.com>
2020-03-31 18:01:54 +0300

    Update src/main/java/core/basesyntax/MatrixOperation.java
    Co-Authored-By: Pavel Kurilyuk <50627236+Tarabarnik@users.noreply.github.com>

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -41,7 +41,7 @@ public class MatrixOperation {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix [i].length; j++) {
                 if (matrix [i][j] > pivot) {
-                    pivot = matrix [i] [j];
+                    pivot = matrix[i][j];
                 }
             }
         }
