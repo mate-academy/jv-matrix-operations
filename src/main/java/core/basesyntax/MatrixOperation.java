6a709a1f1d11d63bb5a1280d2abf42de1356d554
NikolayAbysov <nikolay.abysov@ukr.net>
2020-04-01 09:53:42 +0300

    Redundant check and variable fixed

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -15,23 +15,18 @@ public class MatrixOperation {
 
     public int[] getCounterDiagonal(int[][] matrix) {
         int [] counterDiagonal = new int[matrix[0].length];
-        int counter = matrix[0].length - 1;
         for (int i = 0; i < matrix[0].length; i++) {
-            counterDiagonal[i] = matrix[i][counter];
-            counter--;
+            counterDiagonal[i] = matrix[i][matrix[0].length - i - 1];
         }
         return counterDiagonal;
     }
 
     public int getMin(int[][] matrix) {
         int min = matrix[0][0];
-        if (matrix.length == 1) {
-            return matrix[0][0];
-        }
-        for (int i = 0; i < matrix[0].length; i++) {
-            for (int j = 0; j < matrix[1].length; j++) {
-                if (matrix[i][j] < min) {
-                    min = matrix[i][j];
+        for (int[] arr : matrix) {
+            for (int i : arr){
+                if (i < min){
+                    min = i;
                 }
             }
         }
@@ -40,13 +35,10 @@ public class MatrixOperation {
 
     public int getMax(int[][] matrix) {
         int max = matrix[0][0];
-        if (matrix.length == 1) {
-            return matrix[0][0];
-        }
-        for (int i = 0; i < matrix[0].length; i++) {
-            for (int j = 0; j < matrix[1].length; j++) {
-                if (matrix[i][j] > max) {
-                    max = matrix[i][j];
+        for (int[] arr : matrix) {
+            for (int i : arr){
+                if (i > max){
+                    max = i;
                 }
             }
         }
