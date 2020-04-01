086ec0e75015e41b61513326328ef4557ffb7a03
Djeckkenni <djeck27kenni@gmail.com>
2020-04-01 11:23:51 +0300

    correct code - 2

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -16,43 +16,33 @@ public class MatrixOperation {
 
     public int[] getCounterDiagonal(int[][] matrix) {
         int[] counterDiagonal = new int[matrix.length];
-        int j = matrix.length - 1;
         for (int i = 0; i < matrix.length; i++) {
-            counterDiagonal[i] = matrix[i][j];
-            j--;
+            counterDiagonal[i] = matrix[i][matrix.length - i - 1];
         }
         return counterDiagonal;
     }
 
     public int getMin(int[][] matrix) {
-        int[] min = new int[matrix.length];
+        int min = matrix[0][0];
         for (int i = 0; i < matrix.length; i++) {
-            sortMatrix(matrix[i]);
-            min[i] = matrix[i][0];
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
         }
-        sortMatrix(min);
-        return min[0];
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        int[] max = new int[matrix.length];
+        int max = matrix[0][0];
         for (int i = 0; i < matrix.length; i++) {
-            sortMatrix(matrix[i]);
-            max[i] = matrix[i][matrix.length - 1];
-        }
-        sortMatrix(max);
-        return max[max.length - 1];
-    }
-
-    public int[] sortMatrix(int[] matrix) {
-        int temp = 0;
-        for (int i = 0; i < matrix.length - 1; i++) {
-            if (matrix[i] > matrix[i + 1]) {
-                temp = matrix[i + 1];
-                matrix[i + 1] = matrix[i];
-                matrix[i] = temp;
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
             }
         }
-        return matrix;
+        return max;
     }
 }
