8686657ddde60f621a01e2261b7b6a6da02c360d
Oleksandr Kovaliuk <kovaliukoleksandr@gmail.com>
2020-04-01 16:32:45 +0300

    methods in MatrixOperation class

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -7,27 +7,22 @@ package core.basesyntax;
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
         int[] diagonalArr = new int[matrix.length];
-
         for (int i = 0; i < matrix.length; i++) {
             diagonalArr[i] = matrix[i][i];
         }
-
         return diagonalArr;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
         int[] diagonalArr = new int[matrix.length];
-
         for (int i = 0; i < matrix.length; i++) {
             diagonalArr[i] = matrix[i][matrix.length - i - 1];
         }
-
         return diagonalArr;
     }
 
     public int getMin(int[][] matrix) {
         int min = matrix[0][0];
-
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (min > matrix[i][j]) {
@@ -35,13 +30,11 @@ public class MatrixOperation {
                 }
             }
         }
-
         return min;
     }
 
     public int getMax(int[][] matrix) {
         int max = matrix[0][0];
-
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (max < matrix[i][j]) {
@@ -49,7 +42,6 @@ public class MatrixOperation {
                 }
             }
         }
-
         return max;
     }
 }
