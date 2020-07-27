71a5d108b1fab86c7f2bbebc8c8de34bafd98609
andriylagun <andriy.lagun@gmail.com>
2020-07-27 20:43:05 +0300

    implemented getDiagonal(), getCounterDiagonal(),getMin(), getMax()

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,18 +6,33 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] mainDiagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++)
+            mainDiagonal[i] = matrix[i][i];
+        return mainDiagonal;
     }
 
-    public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+    public static int[] getCounterDiagonal(int[][] matrix) {
+        int[] counterDiagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++)
+            counterDiagonal[i] = matrix[i][matrix.length - i - 1];
+
+        return counterDiagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int[] ints : matrix)
+            for (int j = 0; j < matrix.length; j++)
+                if (ints[j] < min) min = ints[j];
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int[] ints : matrix)
+            for (int j = 0; j < matrix.length; j++)
+                if (ints[j] > max) max = ints[j];
+        return max;
     }
 }
