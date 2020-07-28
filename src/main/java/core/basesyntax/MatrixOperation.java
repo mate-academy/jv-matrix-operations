3cc2455636bbbb4c4fa11e1e696c1fe3948a2624
Nokafan <volodymy.stepura@gmail.com>
2020-07-28 13:03:32 +0300

    Added solution for MatrixOperationTest

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,18 +6,60 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        if (matrix.length == 0) {
+            return new int[0];
+        }
+        if (matrix.length == 1) {
+            return matrix[0];
+        }
+        int[] resultArray = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            resultArray[i] = matrix[i][i];
+        }
+        return resultArray;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        if (matrix.length == 0) {
+            return new int[0];
+        }
+        if (matrix.length == 1) {
+            return matrix[0];
+        }
+        int[] resultArray = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            resultArray[i] = matrix[i][matrix.length - i - 1];
+        }
+        return resultArray;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int result = Integer.MAX_VALUE;
+        for (int[] arrayLine : matrix) {
+            for (int arrayColumn : arrayLine) {
+                if (arrayColumn < result) {
+                    result = arrayColumn;
+                }
+            }
+        }
+        return result;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int result = Integer.MIN_VALUE;
+        for (int[] arrayLine : matrix) {
+            for (int arrayColumn : arrayLine) {
+                if (arrayColumn > result) {
+                    result = arrayColumn;
+                }
+            }
+        }
+        return result;
     }
 }
