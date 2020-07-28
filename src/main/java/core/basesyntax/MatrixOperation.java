2166d11e49b4e85d36933be59de9baf71d1d081b
FYarkiy <flx_jb@yahoo.com>
2020-07-28 13:43:14 +0300

    Solution for diagonals, Max and Min

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,18 +6,38 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] mainDiagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            mainDiagonal[i] = matrix[i][i];
+        }
+        return mainDiagonal;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] secondaryDiagonal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            secondaryDiagonal[i] = matrix[i][matrix.length - 1 - i];
+        }
+        return secondaryDiagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int[] row : matrix) {
+            for (int cell : row) {
+                min = (cell < min) ? min = cell : min;
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int[] row : matrix) {
+            for (int cell : row) {
+                max = (cell > max) ? cell : max;
+            }
+        }
+        return max;
     }
 }
