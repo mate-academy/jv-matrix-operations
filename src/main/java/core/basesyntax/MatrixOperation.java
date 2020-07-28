1afa1461761adfaa6bfcb6fcbcdd42d936622df4
Andriy <andriy.lagun@gmail.com>
2020-07-28 13:35:45 +0300

    Added braces to for and if statement

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -8,31 +8,41 @@ public class MatrixOperation {
 
     public static int[] getCounterDiagonal(int[][] matrix) {
         int[] counterDiagonal = new int[matrix.length];
-        for (int i = 0; i < matrix.length; i++)
+        for (int i = 0; i < matrix.length; i++) {
             counterDiagonal[i] = matrix[i][matrix.length - i - 1];
+        }
         return counterDiagonal;
     }
 
     public int[] getDiagonal(int[][] matrix) {
         int[] mainDiagonal = new int[matrix.length];
-        for (int i = 0; i < matrix.length; i++)
+        for (int i = 0; i < matrix.length; i++) {
             mainDiagonal[i] = matrix[i][i];
+        }
         return mainDiagonal;
     }
 
     public int getMin(int[][] matrix) {
         int min = matrix[0][0];
-        for (int[] ints : matrix)
-            for (int j = 0; j < matrix.length; j++)
-                if (ints[j] < min) min = ints[j];
+        for (int[] ints : matrix) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (ints[j] < min) {
+                    min = ints[j];
+                }
+            }
+        }
         return min;
     }
 
     public int getMax(int[][] matrix) {
         int max = matrix[0][0];
-        for (int[] ints : matrix)
-            for (int j = 0; j < matrix.length; j++)
-                if (ints[j] > max) max = ints[j];
+        for (int[] ints : matrix) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (ints[j] > max) {
+                    max = ints[j];
+                }
+            }
+        }
         return max;
     }
 }
