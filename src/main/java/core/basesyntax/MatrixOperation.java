616f5575bc6706285a33d3bb249ad32785137785
ROMASHSX <andriiromash@gmail.com>
2020-07-28 12:40:08 +0300

    Task is done

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -1,23 +1,47 @@
 package core.basesyntax;
 
-/**
- * Реалізуйте методи для знаходження головної та побічної діагоналі квадратної матриці,
- * максимального та мінімального елементів.
- */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] diagonal = new int[matrix[0].length];
+        for (int i = 0; i < matrix[0].length; i++) {
+            diagonal[i] = matrix[i][i];
+        }
+        return diagonal;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] diagonal = new int[matrix[0].length];
+        for (int j = 0; j < matrix[0].length; j++) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                if (i + j == matrix[0].length - 1) {
+                    diagonal[i] = matrix[i][j];
+                }
+            }
+        }
+        return diagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        return min;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int max = matrix[0][0];
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
     }
 }
