b59b61394306e367fbd57aff2afc11cb1433215a
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-07-28 12:44:32 +0300

    Added solution

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -6,18 +6,43 @@ package core.basesyntax;
  */
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        return null;
+        int[] mainDiagonal = new int[matrix.length];
+        for (int indexOfMatrix = 0; indexOfMatrix < matrix.length; indexOfMatrix++) {
+            mainDiagonal[indexOfMatrix] = matrix[indexOfMatrix][indexOfMatrix];
+        }
+        return mainDiagonal;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        return null;
+        int[] counterDiagonal = new int[matrix.length];
+        for (int indexOfMatrix = 0; indexOfMatrix < matrix.length; indexOfMatrix++) {
+            counterDiagonal[indexOfMatrix] =
+                    matrix[indexOfMatrix][matrix.length - indexOfMatrix - 1];
+        }
+        return counterDiagonal;
     }
 
     public int getMin(int[][] matrix) {
-        return 0;
+        int minValue = Integer.MAX_VALUE;
+        for (int rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < matrix.length; columnIndex++) {
+                if (minValue > matrix[rowIndex][columnIndex]) {
+                    minValue = matrix[rowIndex][columnIndex];
+                }
+            }
+        }
+        return minValue;
     }
 
     public int getMax(int[][] matrix) {
-        return 0;
+        int maxValue = Integer.MIN_VALUE;
+        for (int rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < matrix.length; columnIndex++) {
+                if (maxValue < matrix[rowIndex][columnIndex]) {
+                    maxValue = matrix[rowIndex][columnIndex];
+                }
+            }
+        }
+        return maxValue;
     }
 }
