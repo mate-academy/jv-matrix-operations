e4206a7784c93c5c9a102ab859f7f065e2a9f69c
ROMASHSX <andriiromash@gmail.com>
2020-07-28 14:28:05 +0300

    getCounterDiagonal method is refactored

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -11,12 +11,15 @@ public class MatrixOperation {
 
     public int[] getCounterDiagonal(int[][] matrix) {
         int[] diagonal = new int[matrix[0].length];
-        for (int j = 0; j < matrix[0].length; j++) {
+        /*for (int j = 0; j < matrix[0].length; j++) {
             for (int i = 0; i < matrix[0].length; i++) {
                 if (i + j == matrix[0].length - 1) {
                     diagonal[i] = matrix[i][j];
                 }
             }
+        }*/
+        for (int i = 0; i < matrix[0].length; i++) {
+            diagonal[i] = matrix[i][matrix[0].length - 1 - i];
         }
         return diagonal;
     }
