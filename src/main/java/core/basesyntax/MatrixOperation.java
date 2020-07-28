8da69109e8ebd88b3f56f34cfce7deb64c634910
ROMASHSX <andriiromash@gmail.com>
2020-07-28 14:30:14 +0300

    recheck mvn clean package

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -11,13 +11,6 @@ public class MatrixOperation {
 
     public int[] getCounterDiagonal(int[][] matrix) {
         int[] diagonal = new int[matrix[0].length];
-        /*for (int j = 0; j < matrix[0].length; j++) {
-            for (int i = 0; i < matrix[0].length; i++) {
-                if (i + j == matrix[0].length - 1) {
-                    diagonal[i] = matrix[i][j];
-                }
-            }
-        }*/
         for (int i = 0; i < matrix[0].length; i++) {
             diagonal[i] = matrix[i][matrix[0].length - 1 - i];
         }
